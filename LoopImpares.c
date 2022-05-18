429123701bb8f0f67f46ffc430b298614fb90c1f
Amanda Amorim <amandavsadev@gmail.com>
2022-05-18 14:21:19 -0300

    Repetição

--- LoopImpares.c
+++ LoopImpares.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-// Você pode encontrar o enunciado da questão em: https://thehuxley.com/problem/21?locale=pt_BR
+// VocÃª pode encontrar o enunciado da questÃ£o em: https://thehuxley.com/problem/21?locale=pt_BR
 
 int main(void) 
 {
   int n, m;
-  printf("Digite dois números: \n");
+  
+  printf("Digite dois nÃºmeros: \n");
   scanf("%d%d",&n,&m);
   while (n<m)    
   {
